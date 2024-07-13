350f752142fa2cc8f904d402b520ce268440c58b
Kiran-kapikad <kirankumark4894@gmail.com>
2024-07-13 09:32:47 +0530

    Updated Commit

--- src/ex_07072024/Lab006.java
+++ src/ex_07072024/Lab006.java
@@ -3,5 +3,6 @@ package ex_07072024;
 public class Lab006 {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello");
     }
 }
--- src/ex_07072024/Lab007.java
+++ src/ex_07072024/Lab007.java
@@ -3,5 +3,6 @@ package ex_07072024;
 public class Lab007 {
     public static void main(String[] args) {
         System.out.println("Lab007");
+        System.out.println("Lab007");
     }
 }
