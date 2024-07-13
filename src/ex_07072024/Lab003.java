86fb6c43297e04c33e2de67547c4fa0b63974d80
Kiran-kapikad <kirankumark4894@gmail.com>
2024-07-13 09:01:26 +0530

    Updated Commit

--- src/ex_07072024/Lab003.java
+++ src/ex_07072024/Lab003.java
@@ -1,5 +1,12 @@
 package ex_07072024;
 
 public class Lab003 {
+    public static void main(String[] args) {
+        //Single comment
 
+    /*
+    Multiple comment
+
+     */
+    }
 }
---
+++ src/ex_07072024/Lab005.java
@@ -0,0 +1,10 @@
+package ex_07072024;
+
+public class Lab005 {
+    public static void main(String[] args) {
+        System.out.println("hello");//Ctrl+D to copy the line
+        System.out.println("hello1");
+        System.out.println("hello2");
+        System.out.println("hello2");
+    }
+}
