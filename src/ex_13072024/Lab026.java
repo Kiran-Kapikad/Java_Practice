132a2af24453dffb53dec1e88e8d52f691222d2d
Kiran-kapikad <kirankumark4894@gmail.com>
2024-07-24 07:07:27 +0530

    Updated Commit

--- src/ex_13072024/Lab026.java
+++ src/ex_13072024/Lab026.java
@@ -3,7 +3,7 @@ package ex_13072024;
 public class Lab026 {
     public static void main(String[] args) {
         //Decimal
-//        int age = 10;
+//        int age = 10;//Decimal
 //        int b = 0b1010;//Binary - Automation x
 //        int octal = 0101;//if your automation you may be working with these numbers
 //        int hex = 0Xface;
---
+++ src/ex_13072024/Lab028.java
@@ -0,0 +1,17 @@
+package ex_13072024;
+
+public class Lab028 {
+    public static void main(String[] args) {
+        //Calc
+        double a = 10;
+        double b = 10;
+        double sum = a+b;
+        double sub = a-b;
+        double mul = a*b;
+        double div = a/b;
+        System.out.printf("Sum is %f \n",sum);
+        System.out.printf("Sum is %f \n",sub);
+        System.out.printf("Sum is %f \n",mul);
+        System.out.printf("Sum is %f \n",div);
+    }
+}
