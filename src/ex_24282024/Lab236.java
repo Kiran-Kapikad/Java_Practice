6dd87184d7cf911c26625f037c6105a2d1d7eee9
Kiran-kapikad <kirankumark4894@gmail.com>
2024-10-06 10:34:56 +0530

    Updated Commit

---
+++ src/ex_24282024/Lab236.java
@@ -0,0 +1,12 @@
+package ex_24282024;
+
+import java.util.LinkedList;
+
+public class Lab236 {
+    public static void main(String[] args) {
+        LinkedList l = new LinkedList();
+        l.add("Node 1");
+        l.add("Node 2");
+        l.add("Node 3");
+    }
+}
---
+++ src/ex_24282024/Lab237.java
@@ -0,0 +1,12 @@
+package ex_24282024;
+
+import java.util.Stack;
+
+public class Lab237 {
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.add("Pramod");
+        s.add("Dutta");
+        s.add("Kiran");
+    }
+}
